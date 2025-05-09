b690a92a5d424fad4c8037ccc63df98e6d7d54c5
Jordan <faadefc@gmail.com>
2025-05-08 17:32:22 -0700

    Finish the program.

--- main.py
+++ main.py
@@ -7,47 +7,75 @@ def clear():
     os.system("cls")
 
 
-def addition(a, b):
-    """Execute the addition program."""
+def compute(a, b, operation):
+    """Run the main program."""
     while True:
-        # Get the numbers and answer for the problem.
+        # Get numbers for the problem.
         num1 = randint(a, b)
         num2 = randint(a, b)
-        answer = str(num1 + num2)
 
+        # Get the answer for the problem.
+        if operation == "1":
+            (answer, opp) = addition(num1, num2)
+        elif operation == "2":
+            (answer, opp) = subtraction(num1, num2)
+        elif operation == "3":
+            (answer, opp) = multiplication(num1, num2)
+        elif operation == "4":
+            (answer, opp) = division(num1, num2)
+        else:
+            return None
+        
         # Get the guess from the user.
-        guess = input(f"{num1} + {num2} = ")
+        guess = input(f"{num1} {opp} {num2} = ")
         clear()
 
-        # Let the user go back.
+        # Check if the user wants to go back.
         if guess == "":
-            break
+            return None
+        
+        # Show the answer.
+        print(f"{num1} {opp} {num2} = {answer}")
 
-        print(f"{num1} + {num2} = {answer}")
-
-        # Check the guess to the answer.
+        # Check if the guess is correct.
         if guess == answer:
             print("Correct!")
         else:
             print("Incorrect!")
         
-        # Observe the results.
+        # Observe the correct answer.
         input()
         clear()
 
 
-def subtraction(a, b):
+def addition(num1, num2):
+    """Execute the addition program."""
+    answer = str(num1 + num2)
+    return (answer, "+")
+
+
+def subtraction(num1, num2):
     """Execute the subtraction program."""
+    answer = str(num1 - num2)
+    return (answer, "-")
 
 
-def multiplication(a, b):
+def multiplication(num1, num2):
     """Execute the multiplication program."""
+    answer = str(num1 * num2)
+    return (answer, "*")
 
-def division(a, b):
+
+def division(num1, num2):
     """Execute the division program."""
+    answer = str(num1 // num2) + " r " + str(num1 % num2)
+    return (answer, "/")
+
 
 def main():
     """Execute the program."""
+    clear()
+
     # Choose a lower bound and upper bound.
     while True:
         try:
@@ -58,25 +86,20 @@ def main():
             clear()
             break
 
-        # Choose an operation.
-        print("(1) Addition")
-        print("(2) Subtraction")
-        print("(3) Multiplication")
-        print("(4) Division\n")
-        operation = input("Input: ")
-        clear()
+        # Initialize the loop.
+        operation = " "
 
-        # Run the operation program.
-        if operation == "1":
-            addition(a, b)
-        elif operation == "2":
-            subtraction(a, b)
-        elif operation == "3":
-            multiplication(a, b)
-        elif operation == "4":
-            division(a, b)
-        else:
-            continue
+        while operation != "":
+            # Choose an operation.
+            print("(1) Addition")
+            print("(2) Subtraction")
+            print("(3) Multiplication")
+            print("(4) Division\n")
+            operation = input("Input: ")
+            clear()
+
+            # Run the main program.
+            compute(a, b, operation)
 
 
 main()
